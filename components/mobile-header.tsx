70ecfc2ce97f538e5e0f3e5071cba18f48d14613
dazaga01 <contact@dazaga.net>
2024-07-03 16:17:28 -0400

    mobile header h 60

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -3,7 +3,7 @@ import { MobileSidebar } from "./mobile-sidebar";
 
 export const MobileHeader = () => {
     return (
-        <nav className="lg:hidden px-6 h-[50px] flex items-center bg-white border-b border-slate-300 fixed top-0 w-full z-50">
+        <nav className="lg:hidden px-6 h-[60px] flex items-center bg-white border-b border-slate-300 fixed top-0 w-full z-50">
             <MobileSidebar />
             <div className="flex-grow flex justify-center">
                 <a href="/">
