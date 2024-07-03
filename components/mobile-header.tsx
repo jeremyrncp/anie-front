a75fc67e4b263c4459f1eb7dc659c3635e5824eb
dazaga01 <contact@dazaga.net>
2024-07-03 16:02:36 -0400

    mobile header undo 20px image 10px

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -3,11 +3,11 @@ import { MobileSidebar } from "./mobile-sidebar";
 
 export const MobileHeader = () => {
     return (
-        <nav className="lg:hidden px-6 h-[20px] flex items-center bg-white border-b border-slate-300 fixed top-0 w-full z-50">
+        <nav className="lg:hidden px-6 h-[50px] flex items-center bg-white border-b border-slate-300 fixed top-0 w-full z-50">
             <MobileSidebar />
             <div className="flex-grow flex justify-center">
                 <a href="/">
-                    <Image src="/logodazaga.png" height={35} width={35} alt="Logo" />
+                    <Image src="/logodazaga.png" height={10} width={10} alt="Logo" />
                 </a>
             </div>
         </nav>
