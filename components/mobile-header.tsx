0746f5a667cc4a7d80d5554252d6d5b3bbf0dbd0
dazaga01 <contact@dazaga.net>
2024-07-03 16:16:24 -0400

    40 px img mobile header

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -7,7 +7,7 @@ export const MobileHeader = () => {
             <MobileSidebar />
             <div className="flex-grow flex justify-center">
                 <a href="/">
-                    <Image src="/logodazaga.png" height={50} width={50} alt="Logo" />
+                    <Image src="/logodazaga.png" height={40} width={40} alt="Logo" />
                 </a>
             </div>
         </nav>
