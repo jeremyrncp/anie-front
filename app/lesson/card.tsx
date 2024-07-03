96a2597e125d708f1eec60bb4233cddbad40322c
dazaga01 <contact@dazaga.net>
2024-07-03 15:28:53 -0400

    Original card code

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -58,7 +58,7 @@ export const Card = ({
       {audio}
       {imageSrc && (
         <div
-          className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[80px] w-full"
+          className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full"
         >
           <Image src={imageSrc} fill alt={text} />
         </div>
