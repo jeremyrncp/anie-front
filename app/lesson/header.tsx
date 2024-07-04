438ca73c1ca0dff9883c1d08a65c08e88ef53e28
dazaga01 <contact@dazaga.net>
2024-07-03 20:18:13 -0400

    logo jpg to png

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -19,7 +19,7 @@ export const Header = ({
   return (
     <header className="lg:pt-[50px] pt-[20px] px-10 flex gap-x-7 items-center justify-between max-w-[1140px] mx-auto w-full">
       <Image
-        src="/logodazaga.jpg"
+        src="/logodazaga.png"
         height={25} // Ajustez la taille selon vos besoins
         width={25} // Ajustez la taille selon vos besoins
         alt="Logo Dazaga"
