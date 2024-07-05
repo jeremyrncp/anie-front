f53079a00d3c99ef2ea45b5c7d16a6a919ee591c
dazaga01 <contact@dazaga.net>
2024-07-04 22:07:30 -0400

    undo

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -45,7 +45,7 @@ export const Card = ({
     <div
       onClick={handleClick}
       className={cn(
-        "relative h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
+        "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
         selected && "border-sky-300 bg-sky-100 hover:bg-sky-100",
         selected && status === "correct" 
           && "border-green-300 bg-green-100 hover:bg-green-100",
@@ -57,24 +57,19 @@ export const Card = ({
     >
       {audio}
       {imageSrc && (
-        <div className="flex justify-center items-center">
-          <div
-            className="relative mb-4 max-h-[150px] w-full flex justify-center items-center"
-            style={{ maxHeight: '150px', maxWidth: '100%', overflow: 'hidden' }}
-          >
-            <Image 
-              src={imageSrc} 
-              alt={text} 
-              layout="fill" 
-              objectFit="contain"
-              className="max-h-full max-w-full"
-            />
-          </div>
+        <div
+          className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full"
+        >
+          <Image src={imageSrc} fill alt={text} />
         </div>
       )}
-      <div className="flex flex-col items-center justify-center">
+      <div className={cn(
+        "flex items-center justify-between",
+        type === "ASSIST" && "flex-row-reverse",
+      )}>
+        {type === "ASSIST" && <div />}
         <p className={cn(
-          "text-neutral-600 text-sm lg:text-base text-center",
+          "text-neutral-600 text-sm lg:text-base",
           selected && "text-sky-500",
           selected && status === "correct" 
             && "text-green-500",
@@ -83,8 +78,6 @@ export const Card = ({
         )}>
           {text}
         </p>
-      </div>
-      <div className="absolute bottom-2 right-2">
         <div className={cn(
           "lg:w-[30px] lg:h-[30px] w-[20px] h-[20px] border-2 flex items-center justify-center rounded-lg text-neutral-400 lg:text-[15px] text-xs font-semibold",
           selected && "border-sky-300 text-sky-500",
