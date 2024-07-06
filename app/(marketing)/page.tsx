1789f10e782835f6178713e6597921e935cd7c30
dazaga01 <contact@dazaga.net>
2024-07-06 15:45:26 -0400

    b

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <div className="flex flex-col items-center gap-y-8">
           <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px]">
             Welcome to Dazagara !
-            Learn, practice, and master African languages.
+                 Learn, practice, and master African languages.
           </h1>
           <div className="flex flex-col items-center gap-y-3 max-w-[330px w-full]">
             <ClerkLoading>
